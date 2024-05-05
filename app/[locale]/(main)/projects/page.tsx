06572043c00a2e9421fbd25646d809e351e229aa
MrRabotnik <terbalyantss@gmail.com>
2024-05-05 17:25:47 +0400

    Skeletons added in projects page

--- app/[locale]/(main)/projects/page.tsx
+++ app/[locale]/(main)/projects/page.tsx
@@ -4,6 +4,10 @@ import { useEffect, useState } from "react";
 import "./page.scss";
 import ProjectPreview from "@/Components/ProjectPreview/ProjectPreview";
 import axios from "axios";
+import "react-loading-skeleton/dist/skeleton.css";
+import dynamic from "next/dynamic";
+
+const Skeleton = dynamic(() => import("react-loading-skeleton"));
 
 const ProjectsPage = () => {
     const [projects, setProjects] = useState([]);
@@ -13,7 +17,6 @@ const ProjectsPage = () => {
             .get(`api/project/all`)
             .then((res) => {
                 setProjects(res.data.projects);
-                console.log(res.data.projects);
             })
             .catch((error) => {
                 console.log(error);
@@ -22,16 +25,33 @@ const ProjectsPage = () => {
 
     return (
         <section className="all_projects_section">
-            {projects.length
-                ? projects.map((project: any, index: number) => {
-                      return (
-                          <ProjectPreview
-                              key={index}
-                              project={project}
-                          ></ProjectPreview>
-                      );
-                  })
-                : ""}
+            <div className="container">
+                {projects.length ? (
+                    projects.map((project: any, index: number) => {
+                        return (
+                            <ProjectPreview
+                                key={index}
+                                project={project}
+                            ></ProjectPreview>
+                        );
+                    })
+                ) : (
+                    <>
+                        <Skeleton
+                            highlightColor="rgb(58, 39, 183)"
+                            containerClassName="project-box-skeleton"
+                        />
+                        <Skeleton
+                            highlightColor="rgb(58, 39, 183)"
+                            containerClassName="project-box-skeleton"
+                        />
+                        <Skeleton
+                            highlightColor="rgb(58, 39, 183)"
+                            containerClassName="project-box-skeleton"
+                        />
+                    </>
+                )}
+            </div>
         </section>
     );
 };
